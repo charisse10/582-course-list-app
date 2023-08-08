8ccd710dc04714fc2653296f49a3b2dd06ff079f
Charisse10 <199550@edu.vaniercollege.qc.ca>
2023-08-08 14:41:33 -0400

    editing h1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  <h1>TESTING</h1>
+  <h1>TESTING MAC</h1>
   <h2>List of Courses</h2>
   <h4>Selected Courses: {{ count }}</h4>
   <!-- <CourseItem @outerAdd="AddCourse" /> -->
